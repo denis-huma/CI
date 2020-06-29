23fde982d935998378962f57e6dd80dc7bc7a1ef
Dennis <denys.yaremenko@vakoms.com.ua>
2020-06-29 15:18:19 +0300

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Denis Yaremenko on 29.06.2020.
+//  Copyright © 2020 Denis Yaremenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
